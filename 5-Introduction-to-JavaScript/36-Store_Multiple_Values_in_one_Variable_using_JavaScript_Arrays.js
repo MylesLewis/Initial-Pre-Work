26b5551611f677ee8da144ca81efa43113656c0e
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:19:41 -0600

    Store string and number in variable myArray

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+// Learned about array variables and their ability to store several pieces of data in one placeholder
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Matthew", 42];
