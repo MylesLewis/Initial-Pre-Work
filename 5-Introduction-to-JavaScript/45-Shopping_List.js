71e089a61d33c09ed8a46454e6e90f9fd8298ce5
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:52:02 -0600

    Create a shopping list of almost exclusively jelly beans

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,3 @@
+// Learned how to create a shopping list in the variable myList that is a multi-dimensional array containing several sub-arrays
+
+var myList = [["Jelly Beans", 100], ["More Jelly Beans", 200], ["Even more Jelly Beans", 300], ["The most Jelly Beans", 400], ["Eggs", 12]];
