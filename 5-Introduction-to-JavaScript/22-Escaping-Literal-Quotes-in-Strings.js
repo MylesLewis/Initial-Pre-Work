cb2c5e1f74913dbe67604988c49ef45370facc3a
myleslewis <mylestlewis@gmail.com>
2019-06-29 23:11:04 -0600

    Use escape function to create a string that displays quotes inside of it

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+// Learned about how to use the escape mechanism to distinguish certain characters that javascript considers important to render them just as they area
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
