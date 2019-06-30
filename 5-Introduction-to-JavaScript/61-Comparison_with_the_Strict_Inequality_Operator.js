d09b3bf4882299517dd3bc2dd48b9dae83c98221
myleslewis <mylestlewis@gmail.com>
2019-06-30 13:07:25 -0600

    Change testStrictNotEqual to use strict inequality operator for testing if the value is 17

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+// Learned about the strict inequality operator !== and how it does not convert data types for determinig equality like the strict equality operators
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
