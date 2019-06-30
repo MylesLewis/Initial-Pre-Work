301ed43e9f2a10328321905ad9f584cefa42210a
myleslewis <mylestlewis@gmail.com>
2019-06-30 13:03:49 -0600

    Change compareEquality function if statement to use strict equality operator

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+// Practiced using the equality and strict equality operators. Also learned that you can determine the type of a variable by running "typeof" on the values
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
