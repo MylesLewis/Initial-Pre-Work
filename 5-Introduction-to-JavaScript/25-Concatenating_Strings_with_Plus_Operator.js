fbce552192f3500f656efc40436ebc30cec84eee
myleslewis <mylestlewis@gmail.com>
2019-06-29 23:22:27 -0600

    Concatenate strings into var myStr

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// Learned about how to build new strings out of other strings through using the concatenation operator
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
