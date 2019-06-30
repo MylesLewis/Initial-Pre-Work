cddf4be9aa25087fef40c1a9205a1d1e0b87114d
myleslewis <mylestlewis@gmail.com>
2019-06-30 15:36:41 -0600

    Create object myDog and assign it properties

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,22 @@
+// Learned about objects and how they're similar to arrays but instead of using indexcies to access data you use properties
+// Objects are useful for storing data in a structured ways
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "DogMeat",
+  "legs": 13,
+  "tails": 7,
+  "friends": ["everyone", "anyone who gives pets"]
+
+
+
+};
