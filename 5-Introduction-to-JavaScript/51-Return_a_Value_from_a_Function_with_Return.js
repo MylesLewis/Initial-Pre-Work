a84f85c03895d710be554ae637f70cf33f6ea412
myleslewis <mylestlewis@gmail.com>
2019-06-30 12:34:50 -0600

    Create function timesFive that returns num times 5

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+// Learned about passing values into functions with arguments and return statements to send a value back out of a functions
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
+console.log(timesFive(5));
