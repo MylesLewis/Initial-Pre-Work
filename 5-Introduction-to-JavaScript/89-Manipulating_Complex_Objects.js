607278e2f68f263e218662fdd080265cdf2b2f3a
myleslewis <mylestlewis@gmail.com>
2019-06-30 16:42:43 -0600

    Add new album to myMusic object

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,29 @@
+// Learned how to manipulate data in flexible data structures such as Objects
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+,
+  {
+    "artist": "Radiohead",
+    "title": "OK Computer",
+    "release_year": 1997,
+    "formats": [
+      "CD",
+      "LP",
+      "Web"
+    ]
+
+  }
+
+  ];
