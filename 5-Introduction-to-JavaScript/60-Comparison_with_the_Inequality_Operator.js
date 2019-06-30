f889bf95236bc14dcd24aeb959e6d1611c0eb1e6
myleslewis <mylestlewis@gmail.com>
2019-06-30 13:05:35 -0600

    Use not equal operator to test if value is equal to 99

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// Learned about the inequality operator and it being the opposite of the quality operators
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
