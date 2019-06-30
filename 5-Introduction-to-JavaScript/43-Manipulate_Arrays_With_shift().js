eff3027ffaeb586f5dca939d61409652aeddc6b8
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:33:57 -0600

    Shift off first value from myArray

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,12 @@
+// Learned that pop always removes the last element in an array where as .shift() removes the firstName
+
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
