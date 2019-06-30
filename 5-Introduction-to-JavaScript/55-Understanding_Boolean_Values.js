68102e6f4cf927aa8f5c5c76301e583ca9eed842
myleslewis <mylestlewis@gmail.com>
2019-06-30 12:51:43 -0600

    Change boolean returned to be true instead of flase

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// Learned about the boolean data type and how it can only be either true or false, which act as little on/off switches
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
