bd48a7c6e4b7d7179a5c6b5049c1a862c5e0c9fb
myleslewis <mylestlewis@gmail.com>
2019-06-29 22:39:52 -0600

    Correct vars to use camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,12 @@
+// Learned of the case senitivity of variables and function names
+// Also learned of "camelCase" where the first word of multi-word variables is in lowercase and the first letter of the other words are capitalized.
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
