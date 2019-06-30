a713b8765ec65bc109d38771c43e9d76126c05e1
myleslewis <mylestlewis@gmail.com>
2019-06-30 13:27:43 -0600

    Change order of if/else if statements so that the evalueate in the correct order

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,14 @@
+// Learned that the order of if, and else if statments is important as the function is executed from top to bottom.
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
