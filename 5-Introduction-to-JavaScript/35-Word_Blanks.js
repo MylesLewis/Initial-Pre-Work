c8d9323455c92fde4af6702825fb2b326ec4b749
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:15:54 -0600

    Assemble the word blank setneance uing wordBlanks

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,13 @@
+// Put my knoweldge to the test in a mad libs style game which added a list of delcared variables that I had to assemble into a sentance
+
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "The " + myAdjective + " " + myNoun + " "+ myVerb + " "+ myAdverb + ".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
