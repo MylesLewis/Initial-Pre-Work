08dbf87d646c7a76fde7fd6ad684d97d53af1a52
myleslewis <mylestlewis@gmail.com>
2019-06-29 23:05:18 -0600

    Declare myFirstName and myLastName as my first and last name

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+// Learned how to declare string Variables
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Myles";
+var myLastName = "Lewis";
