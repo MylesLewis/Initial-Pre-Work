6dab9c5909f4e1a8b048a4daac22e7521850cfbc
myleslewis <mylestlewis@gmail.com>
2019-06-29 23:00:17 -0600

    Set vars to use augmented addition

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+// Learned about compound assignments. In this lesson in particular we're using the += operator to increment a variable by a certain amount and set that new amount as the value for the variable.
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a+=12;
+b+=9;
+c+=7;
