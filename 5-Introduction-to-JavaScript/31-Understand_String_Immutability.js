6c91a01160cb6f0defd6a0861224ad925a7bae6d
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:05:01 -0600

    Set myStr to value "Hello World"

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// Learned that in JavaScript string values are immutable. Specifically that you cannot change specific chacters in a string literal using the previuos bracket notation
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
