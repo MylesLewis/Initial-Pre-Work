70f9c466cda4d8bcfb38df28bca182951bc91b4a
myleslewis <mylestlewis@gmail.com>
2019-06-29 22:31:56 -0600

    Initialize vars abc

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+// Learned that variables start with an "undefined" value that you cannot perform mathematical or concatenating functions on
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
