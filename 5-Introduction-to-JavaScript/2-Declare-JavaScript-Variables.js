09a92c9affac6e9e5ddd0a7b4cf35a3f78fd130f
myleslewis <mylestlewis@gmail.com>
2019-06-29 22:17:25 -0600

    Create variable "myName"

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+/* Learned that there are 7 different data types in JavaScript: undefined, null, boolean, streing, symbol, number, and object. Also learned about variables as a way to point to data rather than having the data being used itself */
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
