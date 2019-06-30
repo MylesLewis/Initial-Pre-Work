8516ba6af7a7258131b38a87c99fac5c64a42073
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:02:45 -0600

    Determine first letter of last name and set to var firstLetterOfLastName

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+// Learned that JavaScript starts counting at 0 in what is known as zero-index counting and how to determine the character at a specific location through bracket notation
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
