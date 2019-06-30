d89167840bba89f68642a02b616e543647b11307
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:25:37 -0600

    Modify myArray[0] to have value of 45

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,11 @@
+// Learned how to modify array data through using indexes. Learned that arrays are mutable unlike strings
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
