482c4d83d72a99713acc3e7af2dee5a59b3eae55
myleslewis <mylestlewis@gmail.com>
2019-06-30 16:46:24 -0600

    Assign gloveBoxContents a property inside the nested object car

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// Learned how to access subproperties of objects through chaining together notations
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
