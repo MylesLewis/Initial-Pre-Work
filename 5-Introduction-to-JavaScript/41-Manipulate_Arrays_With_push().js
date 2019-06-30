d04b54c1b2c3435a9cf2023e538c40bebb5c3c34
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:30:55 -0600

    Use push to append to myArray ["dog", 3]

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,13 @@
+// Learned how to manipulate arrays with push() which appends data tot the end of arrays
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+
+myArray.push(["dog", 3]);
