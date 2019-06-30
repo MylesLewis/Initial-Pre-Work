17f790869a6127dabe4e1669d0c2b0b3dff1ee0a
myleslewis <mylestlewis@gmail.com>
2019-06-30 12:59:44 -0600

    Change equality operator to determine if value is equal to 12

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,13 @@
+// Learned about comparison operators and how they return a boolean value of either true or trueOrFalse
+// Relatedly, learned about the equality == operator and how it evaluates whether or not 2 values are equivalent and returns true if they area
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(12);
