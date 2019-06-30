a8811c6784c55d877857d1691403b647a8764a2d
myleslewis <mylestlewis@gmail.com>
2019-06-30 11:06:12 -0600

    Determine 3rd letter of last name and set to thirdLetterOfLastName

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,12 @@
+// Learned how to use brack notation to find the nth character in a string
+
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
