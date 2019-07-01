76bbd69de4a0cc09c2a11bb5284afca2f0c93102
myleslewis <mylestlewis@gmail.com>
2019-06-30 21:26:43 -0600

    Check if a is equal to b and return boolean

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,7 @@
+// Learned about the conditional ternary operator which can be used as a one line if-else expression
+
+function checkEqual(a, b) {
+return a == b ? true : false;
+}
+
+checkEqual(1, 2);
