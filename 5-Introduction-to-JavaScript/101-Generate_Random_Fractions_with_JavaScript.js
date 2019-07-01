04fce1765ebc1e19bad12b01c08ae46773c21a39
myleslewis <mylestlewis@gmail.com>
2019-06-30 21:01:47 -0600

    Change return to use math.random() for a random number

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// Learned about the ability of JavaScript to generate random numbers. This is useful for creating random behavior.
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
