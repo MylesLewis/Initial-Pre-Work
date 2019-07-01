9d81ec88d5f4ca88d7a3abe4cf0384fe8a9dc82c
myleslewis <mylestlewis@gmail.com>
2019-07-01 06:52:56 -0600

    Add default value to value of 1

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,11 @@
+// Learned adding default parameters to functions such that it creates more flexible functionWithArgs
+
+const increment = (function() {
+  "use strict";
+
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
