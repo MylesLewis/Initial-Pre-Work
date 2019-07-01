8c046089efbdc3d87b6f4ed2526c1524ac015bd8
myleslewis <mylestlewis@gmail.com>
2019-06-30 18:12:47 -0600

    Create an array that pushes numbers 0-4 with a while loop

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+// Learned about running the same code multiple times through using while loops.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+
+while (i < 5) {
+  myArray.push(i);
+  i++;
+}
