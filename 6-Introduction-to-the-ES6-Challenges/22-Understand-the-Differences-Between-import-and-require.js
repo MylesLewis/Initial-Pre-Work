05d0d90531248f41e0f24a4d90efff3f5a595408
myleslewis <mylestlewis@gmail.com>
2019-07-01 08:57:24 -0600

    Import capitalizeString from string_functions and use that on 'hello!'

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+// Learned about the streamlined importants of import which is able to grab certain code from external resources
+
+"use strict";
+import { capitalizeString } from "string_functions"
+capitalizeString("hello!");
