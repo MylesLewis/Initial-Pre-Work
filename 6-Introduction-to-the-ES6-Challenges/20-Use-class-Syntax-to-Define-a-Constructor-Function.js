19cd9afa632ede7aef62f88e0fad523392dfcb59
myleslewis <mylestlewis@gmail.com>
2019-07-01 08:21:13 -0600

    Create class vegetable and use to make carrot

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,16 @@
+// Learned about class syntax and using it to define constructor functions
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+class Vegetable{
+  constructor(name){
+    this.name = name;
+  }
+}
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
