a0e9d4404715f54752c9e8fc9ff0d299b8bb4fdc
myleslewis <mylestlewis@gmail.com>
2019-07-01 07:09:21 -0600

    Copy arr1 to arr2

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+// Learned that the spread operator spreads out the contents of an array so that it can be read by other functions that require such evaluates
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
